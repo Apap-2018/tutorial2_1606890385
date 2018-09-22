07b2cdf40bb45f931f9eb00d8ca116833b7efd44
shafira21 <fitrishaff21@gmail.com>
2018-09-22 08:23:29 +0700

    done

--- src/main/java/com/example/demo/controller/PageController.java
+++ src/main/java/com/example/demo/controller/PageController.java
@@ -31,8 +31,42 @@ public class PageController {
 		}
 		return "challenge";
 	}
-	
-	
-	
 
+	@RequestMapping ("/generator")
+	public String generator(@RequestParam(value = "a", defaultValue="0",required = false) int a, @RequestParam(value="b", defaultValue="0",required= false) int b , @RequestParam(value="var", required = false, defaultValue = "hm") String var, Model model) {
+		model.addAttribute("a",a);
+		model.addAttribute("b", b);
+		String hasil = "hm";
+		String res ="";
+		if(a>1) {
+			System.out.println("a>1");
+			for(int i=1;i<a;i++) {
+				hasil+="m";
+			}
+			if(b>1) {
+				System.out.println("masoook");
+				for (int j=1;j<=b;j++) {
+					res += hasil+" ";
+				}
+			}
+			else {
+				res = hasil;
+			}
+		}
+		else if(b>1) {
+			System.out.println("b>1");
+			if(a<=1){
+				for (int j=1;j<=b;j++) {
+					res += hasil+" ";
+				}
+			}
+		}
+		else {
+			res = hasil;
+		}
+		
+		model.addAttribute("var", res);
+		return "generator";
+	}
 }
+	
